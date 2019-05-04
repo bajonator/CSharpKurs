d8364bce6a9e72231e28b9f31bb164f56aadb6dc
bajo123@wp.pl <bajo123@wp.pl>
2019-05-04 19:46:00 +0200

    pierwszy program hello. Tablica args

---
+++ hello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args[0];
+            Console.WriteLine("witaj, " + name);
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("witaj, " + item);
+
+            //}
+        }
+    }
+}
